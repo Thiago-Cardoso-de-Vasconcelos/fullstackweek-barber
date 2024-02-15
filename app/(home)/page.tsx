9dbd2808dc60c7750c5cf55c4e337263e274a56d
Thiago Vasconcelos <thiagocardosodevasconcelos@outlook.com>
2024-02-15 16:38:18 -0300

    feat: add bookings page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,14 @@
 import { format } from 'date-fns';
-import Header from '../_components/Header';
+import Header from '../_components/header';
 import { ptBR } from 'date-fns/locale';
 import Search from './_components/Search';
 import BookingItem from '../_components/booking-item';
 import { db } from '../_lib/prisma';
 import BarbershopItem from './_components/barbershop-item';
 
-
 export default async function Home() {
-    const barbershops = await db.barbershop.findMany({});
-    console.log(barbershops);
-    
+  const barbershops = await db.barbershop.findMany({});
+  console.log(barbershops);
 
   return (
     <div>
@@ -28,10 +26,10 @@ export default async function Home() {
         <Search />
       </div>
       <div className='px-5 mt-6'>
-        <h2 className='text-xs mb-3 uppercase text-gray-400 font-bold'>
+        {/* <h2 className='text-xs mb-3 uppercase text-gray-400 font-bold'>
           Agendamentos
         </h2>
-        <BookingItem />
+        <BookingItem /> */}
       </div>
 
       <div className='mt-6'>
